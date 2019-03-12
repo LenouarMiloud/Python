bdcb05efc8043137f423541fc1977d280dc249d7
miloud1010 <lenouarmiloud@gmail.com>
2019-03-12 23:52:39 +0100

    first program

---
+++ typeOfVariavble.py
@@ -0,0 +1,7 @@
+intnum = 55
+doubnum = 52.36
+strval = "hello"
+
+print (type(intnum))
+print (type(doubnum))
+print (type(strval))
---
+++ useOf_numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = [[5,6,8],[10,25,3.0],[45,23,63]]
+
+arr
+
+
+np.array(arr)
