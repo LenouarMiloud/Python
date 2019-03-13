7c0fc2efb795b5c37c346357fcc1d66f62e3994e
miloud1010 <lenouarmiloud@gmail.com>
2019-03-13 15:42:48 +0100

    add new file

---
+++ inisializeArray.py
@@ -0,0 +1,16 @@
+import numpy as np 
+"""
+ we use the numpy librarie to initialize an array
+ the arange method(start,stop,step)
+
+"""
+# here we initialize an array by the even numbers between 0 and 20
+np.arange(0,20,2)
+# here we initialize an array witch contai 5 zeros  
+np.zeros(5)
+# here we create a matrix of 2 lines and 3 colomns and contain only the zeros
+np.zeros((2,3))
+# an array of 5 ones 
+np.ones(5)
+# matrix of identity
+np.eye(5)
